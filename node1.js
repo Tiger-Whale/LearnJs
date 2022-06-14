cf4abca2776da75af5818b94b0ad4ab48f0a8994
Lance Sun <lance.sun@hatchbetter.com>
2022-06-14 14:12:47 +0800

    add nodejs learn

--- node1.js
+++ node1.js
@@ -1,8 +1,21 @@
-let http = require("http");
-http
-  .createServer(function (req, rep) {
-    rep.writeHead(200, { "Content-Type": "text/plain" });
-    rep.end("Hello World");
-  })
-  .listen(8888);
-console.log("test server on 8888");
+// let http = require("http");
+// http
+//   .createServer(function (req, rep) {
+//     rep.writeHead(200, { "Content-Type": "text/plain" });
+//     rep.end("Hello World");
+//   })
+//   .listen(8888);
+// console.log("test server on 8888");
+
+//no sync
+let fs = require("fs");
+let data = fs.readFileSync("./1.html");
+console.log("data", data.toString);
+console.log("Project End");
+
+//sync
+fs.readFile("./1.html", function (error, data) {
+  if (error) return console.log(error);
+  console.log("data", data.toString);
+});
+console.log("Project End");
